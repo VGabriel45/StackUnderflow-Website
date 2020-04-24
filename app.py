a519ec4f7f02a1c60349fc7f141003e8f2624ab0
Gabi <gabriel_marian22@yahoo.com>
2020-04-24 14:06:08 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,112 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+response_like = 0
+question_like = 0
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    likes = db.relationship('Likes', backref='question')
+    responses = db.relationship('Response', backref='question')
+
+
+class Response(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    response = db.Column(db.Text(), nullable=True)
+    author = db.Column(db.String(100), nullable=False)
+    likes = db.relationship('Likes', backref='response')
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+
+
+class Likes(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    number_of_likes = db.Column(db.Integer, nullable=True)
+    response_id = db.Column(db.Integer, db.ForeignKey('response.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        author = request.form['author']
+        new_question = Question(
+            title=title, description=description, author=author)
+        db.session.add(new_question)
+        db.session.commit()
+        print('Added')
+        return redirect('/')
+    else:
+        all_questions = Question.query.all()
+        all_responses = Response.query.all()
+        return render_template('index.html', all_questions=all_questions, all_responses=all_responses)
+
+
+@app.route('/ask-question', methods=['GET', 'POST'])
+def ask():
+    return render_template('form.html')
+
+
+@app.route('/respond-question/<id>', methods=['GET', 'POST'])
+def respond_to_question(id):
+    question = Question.query.get_or_404(id)
+    if request.method == 'POST':
+        response = request.form['response']
+        author = request.form['author']
+        new_response = Response(
+            response=response, author=author, question=question)
+        db.session.add(new_response)
+        db.session.commit()
+        return redirect(f'/respond-question/{id}')
+    else:
+        return render_template('respond.html', question=question)
+
+
+@app.route('/like-question/<id>')
+def like_question(id):
+    global question_like
+    question = Question.query.get_or_404(id)
+    question_like += 1
+    Likes(number_of_likes=question_like, question=question)
+    db.session.commit()
+    return redirect(request.referrer)
+
+
+@app.route('/like-response/<id>')
+def like_response(id):
+    global response_like
+    response = Response.query.get_or_404(id)
+    response_like += 1
+    Likes(number_of_likes=response_like, response=response)
+    db.session.commit()
+    return redirect(request.referrer)
+
+
+@app.route('/delete-response/<id>')
+def delete_response(id):
+    response = Response.query.get_or_404(id)
+    db.session.delete(response)
+    db.session.commit()
+    return redirect(request.referrer)
+
+
+@app.route('/delete-question/<id>')
+def delete_question(id):
+    question = Question.query.get_or_404(id)
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(request.referrer)
+
+
+if __name__ == "__main__":
+    debug = True
+    app.run()
